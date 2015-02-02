7cfb6f3519e2274271691355cb3d3183b6cd77eb
Hiroshi Ota <otahi.pub@gmail.com>
2015-02-03 06:47:56 +0900

    Update lxc installation

--- provision.sh
+++ provision.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 ### install packages
-yum -y --nogpgcheck localinstall share/RPMS/centos64/x86_64/lxc-0.9.0-1.el6.x86_64.rpm \
-    share/RPMS/centos64/x86_64/lxc-libs-0.9.0-1.el6.x86_64.rpm
-yum install -y libcgroup
+rpm -ivh http://mirror.nl.leaseweb.net/epel/6/i386/epel-release-6-8.noarch.rpm
+yum install -y lxc lxc-libs lxc-templates bridge-utils libcgroup
 
 ### virbr0
 cat << EOF > /etc/sysconfig/network-scripts/ifcfg-virbr0
@@ -18,16 +17,10 @@ grep -q virbr0 /etc/sysconfig/network-scripts/ifcfg-eth0 || \
 service network reload
 
 ### lxc
-mkdir -p /cgroup
-grep -q lxc /etc/fstab  || echo "lxc /cgroup cgroup defaults 0 0" >> /etc/fstab
-mount /cgroup
-service cgconfig restart && chkconfig cgconfig on
-
-### lxc centos
-curl -O -L https://gist.github.com/hagix9/3514296/raw/7f6bb4e291fad1dad59a49a5c02f78642bb99a45/lxc-centos
-cp lxc-centos /usr/share/lxc/templates/
-chmod +x /usr/share/lxc/templates/lxc-centos
-sed -i 's/curl -f/curl -Lf/g' /usr/share/lxc/templates/lxc-centos
+service cgconfig start
+service cgred start
+chkconfig --level 345 cgconfig on
+chkconfig --level 345 cgred on
 
 ### lxc vms
 lxc-create -t centos -n vm1 -f share/lxc-vm1.conf
@@ -40,8 +33,8 @@ lxc-start -d -n vm1
 
 ### hints
 echo "##HINTS##"
-echo "You can login to vm1 (with root/password)"
+echo "You can login to vm1 (with root/`cat /var/lib/lxc/vm1/tmp_root_pass`)"
 echo "% sudo lxc-console -n vm1"
-echo "You can login to vm2 (with root/password)"
+echo "You can login to vm2 (with root/`cat /var/lib/lxc/vm2/tmp_root_pass`)"
 echo "% sudo lxc-console -n vm2"
 
