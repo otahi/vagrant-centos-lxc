0e4afc6c48336884d6b134c5b038b4fd119fa3ef
Hiroshi Ota <otahi.pub@gmail.com>
2015-02-08 06:38:38 +0900

    Change provision.sh to specify VMs to create

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,8 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "hfm4/centos6"
-  config.vm.synced_folder "./", "/home/vagrant/share"
-  config.vm.provision :shell, :path => "provision.sh"
+Vagrant.configure('2') do |config|
+  config.vm.box = 'hfm4/centos6'
+  config.vm.provision :shell do |s|
+    s.path = 'provision.sh'
+    vms = %w(db app)
+    s.args =  vms
+  end
 end
--- provision.sh
+++ provision.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+VMS="vm1 vm2"
+if [ -n "$*" ] ; then
+    VMS=$*
+fi
+
 ### install packages
-rpm -ivh http://mirror.nl.leaseweb.net/epel/6/i386/epel-release-6-8.noarch.rpm
-yum install -y lxc lxc-libs lxc-templates bridge-utils libcgroup
+rpm -q epel-release-6-8 || rpm -ivh http://mirror.nl.leaseweb.net/epel/6/i386/epel-release-6-8.noarch.rpm
+rpm -q lxc || yum install -y lxc lxc-libs lxc-templates bridge-utils libcgroup
 
 ### virbr0
 cat << EOF > /etc/sysconfig/network-scripts/ifcfg-virbr0
@@ -17,24 +22,64 @@ grep -q virbr0 /etc/sysconfig/network-scripts/ifcfg-eth0 || \
 service network reload
 
 ### lxc
-service cgconfig start
-service cgred start
+service cgconfig status || service cgconfig start
+service cgred status || service cgred start
 chkconfig --level 345 cgconfig on
 chkconfig --level 345 cgred on
 
-### lxc vms
-lxc-create -t centos -n vm1 -f share/lxc-vm1.conf
-lxc-start -d -n vm2
-sleep 10
+## host config
+cat <<EOF > /home/vagrant/.ssh/config
+Host $VMS
+   StrictHostKeyChecking no
+   UserKnownHostsFile /dev/null
+   IdentityFile /vagrant/.vagrant/machines/default/virtualbox/private_key
+EOF
 
-lxc-create -t centos -n vm2 -f share/lxc-vm2.conf
-lxc-start -d -n vm1
+chown -R vagrant:vagrant /home/vagrant/.ssh/config
+chmod -R 600 /home/vagrant/.ssh/config
+
+cat <<EOF > /etc/hosts
+127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4
+::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
+EOF
+
+## containers
+for vm in $VMS
+do
+    ### destroy lxc
+    lxc-info -n $vm &> /dev/null && lxc-destroy -n $vm -f
+    
+    ### create lxc
+    cat <<EOF > lxc-$vm.conf
+lxc.network.type = veth
+lxc.network.link = virbr0
+lxc.network.flags = up
+lxc.network.veth.pair = veth-$vm
+EOF
+    lxc-create -t centos -n $vm -f lxc-$vm.conf
+
+    ### override lxc container with vagrant
+    ROOTFS=/var/lib/lxc/$vm/rootfs
+    chroot $ROOTFS groupadd -g 1001 vagrant
+    chroot $ROOTFS useradd --create-home -s /bin/bash -u 1001 -g vagrant vagrant
+    mkdir -p $ROOTFS/etc/sudoers.d/
+    cp /etc/sudoers.d/vagrant $ROOTFS/etc/sudoers.d/vagrant
+    cp -r /home/vagrant $ROOTFS/home
+    chown -R vagrant:vagrant $ROOTFS/home/vagrant
+    cp /etc/sudoers $ROOTFS/etc/sudoers
+
+    ### start lxc
+    lxc-start -d -n $vm
+    sleep 10
+
+    echo `lxc-info -n $vm -i -H` $vm >> /etc/hosts
+done
 
 
-### hints
-echo "##HINTS##"
-echo "You can login to vm1 (with root/`cat /var/lib/lxc/vm1/tmp_root_pass`)"
-echo "% sudo lxc-console -n vm1"
-echo "You can login to vm2 (with root/`cat /var/lib/lxc/vm2/tmp_root_pass`)"
-echo "% sudo lxc-console -n vm2"
-
+## hints
+echo "You can login to $VMS with ssh"
+for vm in $VMS
+do
+    echo "You can login to $vm (with root/`cat /var/lib/lxc/$vm/tmp_root_pass`)"
+    echo "sudo lxc-console -n $vm"
+done
